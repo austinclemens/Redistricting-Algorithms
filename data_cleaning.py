54a8e74a29822173befdfff7b1ece084e40d6235
Austin Clemens <austinc@mac.com>
2016-01-17 23:28:27 -0500

    some progress

--- data_cleaning.py
+++ data_cleaning.py
@@ -73,12 +73,53 @@ def load_algorithm_blocks():
 	temp_pandas=pd.DataFrame(master_list,columns=['BlockID','HouseDistrict','state'])
 	return temp_pandas
 
-# Perform the following in order:
-# a=block_vd_pandas()
-# b=load_algorithm_blocks()
-# cd=pd.merge(a,b,on='BlockID')
-# delete all county/district/state duplicates
+def load_precinct_data():
+	files=os.listdir('/Users/austinc/Desktop/Current Work/Redistricting-Algorithms/Raw Data/precinct_votes/')
+	stem='/Users/austinc/Desktop/Current Work/Redistricting-Algorithms/Raw Data/precinct_votes/'
 
+	master_list=[]
+	files=[file for file in files if file[-3:]=='tab']
+
+	for name in files:
+		state=name[0:2]
+		year=name[3:7]
+		name=stem+name
+		print name
+		with open(name,'rb') as file:
+			reader=csv.reader(file)
+			temp=[row for row in reader]
+
+		for row in temp:
+			row.append(state)
+			master_list.append(row)	
+
+	temp_pandas=pd.DataFrame(master_list,columns=['BlockID','HouseDistrict','state'])
+	return temp_pandas	
+
+def full_script():
+	a=block_vd_pandas()
+	b=load_algorithm_blocks()
+	cd=pd.merge(a,b,on='BlockID')
+
+	# need to create a column that tracks how many blocks are in each district - a count of duplicates
+	# and then, when you get rid of blocks and boil the data set down to algorithm/district combos, need
+	# to know how many blocks from a given district are in a given HouseDistrict
+
+	# Count county/district/state duplicates AND county/district/state/algorithm district dupes
+	cd['blocks']=cd.groupby(['state_x','CountyFP','District'])['BlockID'].transform('count')
+	cd['ADblocks']=cd.groupby(['state_x','CountyFP','District','HouseDistrict'])['BlockID'].transform('count')
+
+	# delete all county/district/state duplicates
+	k=cd[['CountyFP','District','state_x','HouseDistrict']].duplicated()
+	i=cd[~k]
+	i['percent_district_in_AlgoHouseDist']=i['ADblocks']/i['blocks']
+
+	# so now you have a nice thing: 186k row matrix of VTDs matched to algorithm districts.
+	# at this point, KY, RI, and OR have been dropped because of the lack of block to VTD
+	# data for them.
+
+	# time to bring in the precinct voting data.
+	votes=
 
 
 
