1bd12c86771d1079f3906b042786753dc9a0dc28
Austin Clemens <austinc@mac.com>
2016-01-24 12:12:22 -0500

    ugh

--- data_cleaning.py
+++ data_cleaning.py
@@ -89,14 +89,40 @@ def load_precinct_data():
 			reader=csv.reader(file)
 			temp=[row for row in reader]
 
+		# it looks like precinct names are *not* consistent between years. Ugh.
+		wanted_columns=['state','year','precinct_code','precinct']
 		column_indices=[]
 
 		for row in temp:
 			row.append(state)
 			master_list.append(row)	
 
-	temp_pandas=pd.DataFrame(master_list,columns=['BlockID','HouseDistrict','state'])
+	temp_pandas=pd.DataFrame(master_list,columns=['BlockID','county','HouseDistrict','state'])
 	return temp_pandas	
+
+def precinct_names():
+	files=os.listdir('/Users/austinc/Desktop/Current Work/Redistricting-Algorithms/Raw Data/Precinct to voting district/')
+	stem='/Users/austinc/Desktop/Current Work/Redistricting-Algorithms/Raw Data/Precinct to voting district/'
+
+	master_list=[]
+	files=[file for file in files if file[-3:]=='txt']
+
+	for name in files:
+		state=name[11:13]
+		name=stem+name
+		print name
+		with open(name,'rb') as file:
+			reader=csv.reader(file, delimiter='|')
+			temp=[row for row in reader]
+
+		temp=temp[1:]
+		for row in temp:
+			row.append(state)
+			master_list.append(row)	
+
+	# for row in master_list
+	temp_pandas=pd.DataFrame(master_list,columns=['StateFP','CountyFP','District','Name','Namelsad','state_x'])
+	return temp_pandas
 
 def full_script():
 	a=block_vd_pandas()
@@ -115,17 +141,86 @@ def full_script():
 	k=cd[['CountyFP','District','state_x','HouseDistrict']].duplicated()
 	i=cd[~k]
 	i['percent_district_in_AlgoHouseDist']=i['ADblocks']/i['blocks']
+	del i['state_y']
+
+	# Merge in the names of precincts
+	c=precinct_names()
+	cid=pd.merge(i,c,on=['state_x','CountyFP','District'])
+
+	# Merge in the names of counties by FIPS
+	fip_file='/Users/austinc/Desktop/Current Work/Redistricting-Algorithms/Raw Data/national_county.csv'
+
+	with open(fip_file,'rb') as file:
+			reader=csv.reader(file)
+			temp=[row[1:4] for row in reader]
+
+	fips=pd.DataFrame(temp,columns=['StateFP','CountyFP','County'])
+	cid=pd.merge(cid,fips,on=['StateFP','CountyFP'])
+
+	# big re-ordering of CID just to make it a little cleaner
+	cid=cid[['state_x','StateFP','County','CountyFP','District','Name','Namelsad','BlockID','HouseDistrict','blocks','ADblocks','percent_district_in_AlgoHouseDist']]
 
 	# so now you have a nice thing: 186k row matrix of VTDs matched to algorithm districts.
-	# at this point, KY, RI, and OR have been dropped because of the lack of block to VTD
-	# data for them.
+	# The problem is that the Harvard file are not well-aligned with the census files, so
+	# to match them, we have to do something like get all districts that are in Alabama in
+	# CountyFP 001, then fuzzy match on district code, name, and namelsad. 
 
-	# time to bring in the precinct voting data.
 	votes=
 
 
 
+	# State by state breakdown of the voting files:
 
+	# AK: 1 district
+	# AL: Name/namelsad is a very close match for harvard 'precinct' (use with county)
+	# AZ: Combination of County Name + District. ex: district=54, county=Rock point, harvard='54 Rock Point - Prec #: 54.1' 
+	# AR: Name is a very close match for harvard 'precinct' (use with county) - some absentee
+	# CA: tricky - statefips+countyfips gives harvard county_code. The rest is completely impenetrable. I have no idea.
+	# CO: 'District' matches the harvard 'vtd' field
+	# CT: Harvard data is aggregated by something other than voting district. Unuseable.
+	# DE: 1 district
+	# FL: County is represented by first three characters in Harvard's 'county' ie Volusia becomes VOL, precinct is that prefix ('VOL') + District
+	# GA: County and District match 'county' and 'precinct' in Harvard
+	# HI
+	# ID
+	# IL
+	# IN
+	# IA
+	# KS
+	# KY
+	# LA
+	# ME
+	# MD
+	# MA
+	# MI
+	# MN
+	# MS
+	# MO
+	# MT
+	# NE
+	# NV
+	# NH
+	# NJ
+	# NM
+	# NY
+	# NC
+	# ND
+	# OH
+	# OK
+	# OR
+	# PA
+	# RI
+	# SC
+	# SD
+	# TN
+	# TX
+	# UT
+	# VT
+	# VA
+	# WA
+	# WV
+	# WI: 
+	# WY: 1 district
 
 
 
